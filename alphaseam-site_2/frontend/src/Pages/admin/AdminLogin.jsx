a6ece02791fbbc6b8216b0ecc76c3c7220eb6810
TejasMahakalAlpha <tejasmahakal@alphaseam.com>
2025-07-10 11:44:23 +0530

    Update AdminLogin.jsx

--- alphaseam-site_2/frontend/src/Pages/admin/AdminLogin.jsx
+++ alphaseam-site_2/frontend/src/Pages/admin/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './admin.css';
+import './Admin.css';
 
 const AdminLogin = () => {
   const [email, setEmail] = useState('');
